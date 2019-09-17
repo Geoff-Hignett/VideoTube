5cb570058e1102597858abb45bd1cb289619f378
Geoff-Hignett <geoffhig@hotmail.com>
2019-09-17 21:12:39 +0100

    config.php added

--- includes/classes/videoDetailsFormProvider.php
+++ includes/classes/videoDetailsFormProvider.php
@@ -6,9 +6,13 @@ class VideoDetailsFormProvider
     {
         $fileInput = $this->createFileInput();
         $titleInput = $this->createTitleInput();
+        $descriptionInput = $this->createDescriptionInput();
+        $privacyInput = $this->createPrivacyInput();
         return "<form action='processing.php' method='post'>
                     $fileInput
                     $titleInput
+                    $descriptionInput
+                    $privacyInput
                 </form>";
     }
 
@@ -16,7 +20,7 @@ class VideoDetailsFormProvider
     {
         return "<div class='form-group'>
                     <label for='exampleFormControlFile1'>Your file</label>
-                    <input type='file' class='form-control-file' id='exampleFormControlFile1' name='fileInput' required>
+                    <input type='file' class='form-control-file' id='exampleFormControlFile1' name='fileInput'>
                 </div>";
     }
 
@@ -24,6 +28,23 @@ class VideoDetailsFormProvider
     {
         return "<div class='form-group'>
                     <input class='form-control' type='text' placeholder='Title' name='titleInput' required>
+                </div>";
+    }
+
+    private function createDescriptionInput()
+    {
+        return "<div class='form-group'>
+                    <textarea class='form-control' placeholder='Description' name='descriptionInput'rows='3'></textarea>
+                </div>";
+    }
+
+    private function createPrivacyInput()
+    {
+        return "<div class='form-group'>
+                    <select class='form-control' name='privacyInput'>
+                        <option value='0'>Public</option>
+                        <option value='1'>Private</option>
+                    </select>
                 </div>";
     }
 }
